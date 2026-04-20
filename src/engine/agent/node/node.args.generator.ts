1316e8a1b4995907b0964345e43bcc5d757dcb6b
Joonmin Park <neopjm109@gmail.com>
2026-04-20 15:01:16 +0900

    Refactor argsGeneratorNode to use response data

--- src/engine/agent/node/node.args.generator.ts
+++ src/engine/agent/node/node.args.generator.ts
@@ -1,20 +1,40 @@
+import { zodResponseFormat } from "openai/helpers/zod.js";
+import Client from "../../client/client";
 import { ArgsGenerator, Task } from "../types";
+import { ArgsGeneratorSchema } from "../schemas";
 
 const argsGeneratorNode = async (
+  client: Client,
   task: Task,
   toolName: string,
 ): Promise<ArgsGenerator> => {
+  const now = new Date();
+  const response = await client.chat({
+    messages: [
+      { role: 'system', content: ""},
+      { role: 'user', content: "" }
+    ],
+    format: zodResponseFormat(ArgsGeneratorSchema, "planner_schema")
+  });
+
+  console.log("----------");
+  console.log(response.choices[0].message?.content);
+  console.log("----------");
+
+  const generator: any = JSON.parse(response.choices[0].message?.content!!);
+  const usage = response.usage;
+
   return {
-    taskId: "",
-    thought: "",
-    args: {},
+    taskId: generator.taskId,
+    thought: generator.thoguth,
+    args: generator.args,
     tokenUsage: {
-      promptTokens: 0,
-      completionTokens: 0,
-      totalTokens: 0,
-      estimatedCost: 0,
+      promptTokens: usage?.prompt_tokens || 0,
+      completionTokens: usage?.completion_tokens || 0,
+      totalTokens: usage?.total_tokens || 0,
+      estimatedCost: usage?.total_tokens || 0 * 0.01,
     },
-    duration: 0,
+    duration: now.getTime() - new Date().getTime(),
     completedAt: new Date(),
   };
 };
