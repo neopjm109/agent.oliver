dfbf8096d82abb0d4131c82d274239fc2455205f
Joonmin Park <neopjm109@gmail.com>
2026-04-20 15:01:26 +0900

    Refactor observationNode to use response data

--- src/engine/agent/node/node.observation.ts
+++ src/engine/agent/node/node.observation.ts
@@ -1,20 +1,40 @@
+import { zodResponseFormat } from "openai/helpers/zod.js";
+import Client from "../../client/client";
+import { ObservationSchema } from "../schemas";
 import { ToolResult } from "../tool/types";
 import { Observation } from "../types";
 
 const observationNode = async (
+  client: Client,
   toolResult: ToolResult,
 ): Promise<Observation> => {
+  const now = new Date();
+  const response = await client.chat({
+    messages: [
+      { role: 'system', content: ""},
+      { role: 'user', content: "" }
+    ],
+    format: zodResponseFormat(ObservationSchema, "planner_schema")
+  });
+
+  console.log("----------");
+  console.log(response.choices[0].message?.content);
+  console.log("----------");
+
+  const observation: any = JSON.parse(response.choices[0].message?.content!!);
+  const usage = response.usage;
+
   return {
-    taskId: "",
-    thought: "",
-    nextStep: "continue",
+    taskId: observation.taskId,
+    thought: observation.thought,
+    nextStep: observation.nextStep || "continue",
     tokenUsage: {
-      promptTokens: 0,
-      completionTokens: 0,
-      totalTokens: 0,
-      estimatedCost: 0,
+      promptTokens: usage?.prompt_tokens || 0,
+      completionTokens: usage?.completion_tokens || 0,
+      totalTokens: usage?.total_tokens || 0,
+      estimatedCost: usage?.total_tokens || 0 * 0.01,
     },
-    duration: 0,
+    duration: now.getTime() - new Date().getTime(),
     completedAt: new Date(),
   };
 };
