6b6a490edecd5db10380222d83f437ad25aaf749
Joonmin Park <neopjm109@gmail.com>
2026-04-20 15:03:24 +0900

    Refactor Agent class to use Client instance

--- src/engine/agent/agent.ts
+++ src/engine/agent/agent.ts
@@ -1,17 +1,20 @@
+import Client from "../client/client";
 import argsGeneratorNode from "./node/node.args.generator";
 import observationNode from "./node/node.observation";
 import plannerNode from "./node/node.planner";
-import summarizerNode from "./node/node.Summarizer";
+import summarizerNode from "./node/node.summarizer";
 import toolExecutorNode from "./node/node.tool.executor";
 import toolSelectorNode from "./node/node.tool.selector";
 import { Tool } from "./tool/types";
 import { AgentState } from "./types";
 
 class Agent {
+  private client: Client;
   private state: AgentState;
   private tools: Tool[];
 
   constructor(tools: Tool[]) {
+    this.client = new Client();
     this.state = {
       sessionId: new Date().getTime().toString(),
       status: "idle",
@@ -33,7 +36,7 @@ class Agent {
   async run(input: string): Promise<string> {
     // Plan
     this.state.status = "planning";
-    const planner = await plannerNode(input);
+    const planner = await plannerNode(this.client, input);
     this.state.tasks = planner.tasks;
 
     // Excute
@@ -42,22 +45,25 @@ class Agent {
       const currentTask = this.state.tasks[this.state.currentStep];
 
       // Tool Selector
-      const selector = await toolSelectorNode(currentTask, this.tools);
+      const selector = await toolSelectorNode(this.client, currentTask, this.tools);
       this.state.results.push(selector);
 
       // Args Generator
-      const generator = await argsGeneratorNode(currentTask, selector.toolName);
+      const generator = await argsGeneratorNode(this.client, currentTask, selector.toolName);
       this.state.results.push(generator);
 
       // Tool Executor
       const executor = await toolExecutorNode(
+        this.client,
+        currentTask.taskId,
+        this.tools,
         selector.toolName,
         generator.args,
       );
       this.state.results.push(executor);
 
       // Observation
-      const observation = await observationNode(executor.result);
+      const observation = await observationNode(this.client, executor.result);
       this.state.results.push(observation);
 
       if (observation.nextStep === "break") {
@@ -73,7 +79,7 @@ class Agent {
 
     // Summarize
     this.state.status = "summarizing";
-    const summarizer = await summarizerNode(this.state);
+    const summarizer = await summarizerNode(this.client, this.state);
     this.state.results.push(summarizer);
 
     this.state.status = "completed";
